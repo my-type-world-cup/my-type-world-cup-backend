b3e9a0e50fddc58b05ace19b9f77f7f9fe0e6418
Ksiyeong <yuiop2767@naver.com>
2023-04-15 22:19:15 +0900

    [chore] 프로젝트 세팅

---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/MytypeworldcupApplication.java
@@ -0,0 +1,13 @@
+package com.mytypeworldcup.mytypeworldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytypeworldcupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytypeworldcupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytypeworldcup/mytypeworldcup/MytypeworldcupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytypeworldcup.mytypeworldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytypeworldcupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
