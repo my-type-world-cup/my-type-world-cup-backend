e194f84062a6005e29420f657a9f90485a87418c
Ksiyeong <yuiop2767@naver.com>
2023-04-21 01:39:39 +0900

    [refactor] Member 수정
    기타 필요한 필드 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/member/entity/Member.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/member/entity/Member.java
@@ -1,16 +1,19 @@
 package com.mytypeworldcup.mytypeworldcup.domain.member.entity;
 
+import com.mytypeworldcup.mytypeworldcup.global.auth.oauth2.dto.ProviderType;
 import com.mytypeworldcup.mytypeworldcup.global.common.Auditable;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
 public class Member extends Auditable {
     @Id
@@ -22,16 +25,23 @@ public class Member extends Auditable {
 
     private String nickname;
 
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProviderType providerType;
+
     @ElementCollection(fetch = FetchType.EAGER)
+    @Column(nullable = false)
     private List<String> roles = new ArrayList<>();
 
     @Builder
-    public Member(String email, String nickname) {
+    public Member(String email, String nickname, String password, ProviderType providerType, List<String> roles) {
         this.email = email;
         this.nickname = nickname;
-    }
-
-    public void setRoles(List<String> roles) {
+        this.password = password;
+        this.providerType = providerType;
         this.roles = roles;
     }
 }
