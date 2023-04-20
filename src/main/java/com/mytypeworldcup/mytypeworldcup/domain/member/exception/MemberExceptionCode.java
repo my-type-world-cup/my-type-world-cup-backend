a0b2faa22a7ba4b0f4d692a01739f37350db506b
Ksiyeong <yuiop2767@naver.com>
2023-04-21 01:42:44 +0900

    [refactor] ExceptionCode추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/member/exception/MemberExceptionCode.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/member/exception/MemberExceptionCode.java
@@ -7,7 +7,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MemberExceptionCode implements ExceptionCode {
-    MEMBER_NOT_FOUND(404, "요청하신 회원을 찾을 수 없습니다.");
+    MEMBER_NOT_FOUND(404, "요청하신 회원을 찾을 수 없습니다."),
+    MEMBER_EXISTS(409, "이미 존재하는 회원입니다."),
+    경고합니다(400,"테스트용 경고입니다");
+
     private int status;
 
     private String message;
