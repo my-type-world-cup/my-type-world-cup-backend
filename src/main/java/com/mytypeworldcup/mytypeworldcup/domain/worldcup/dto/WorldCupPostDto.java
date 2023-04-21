05286d13aefe98586de024daa1e538d7422780ac
Ksiyeong <yuiop2767@naver.com>
2023-04-21 20:23:56 +0900

    [feat] WorldCupDto 추가

---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/dto/WorldCupPostDto.java
@@ -0,0 +1,50 @@
+package com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto;
+
+import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePostDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+public class WorldCupPostDto {
+
+    @Setter
+    private Long memberId;
+
+    @NotBlank
+    private String title; // 제목이므로 공백 불가능
+
+    @NotNull
+    private String description; // 생략가능
+
+    @NotNull
+    private Boolean visibility;
+
+    private String password;
+
+    @NotNull
+    private List<CandidatePostDto> candidatePostDtos;
+
+    @Builder
+    public WorldCupPostDto(String title,
+                           String description,
+                           Boolean visibility,
+                           String password,
+                           List<CandidatePostDto> candidatePostDtos) {
+        this.title = title;
+        this.description = description;
+        this.visibility = visibility;
+        this.password = password;
+        this.candidatePostDtos = candidatePostDtos;
+    }
+
+    public void setWorldCupIdForCandidatePostDtos(Long worldCupId) {
+        candidatePostDtos.stream()
+                .forEach(candidatePostDto -> candidatePostDto.setWorldCupId(worldCupId));
+    }
+
+}
