78dd621a3a62079475c26debf31b0bdfa1f14131
Ksiyeong <yuiop2767@naver.com>
2023-04-21 22:01:15 +0900

    [feat] worldCup 데이터 액세스 관련 추가
    WorldCupRepository, WorldCupMapper 추가

---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/dto/WorldCupMapper.java
@@ -0,0 +1,13 @@
+package com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto;
+
+import com.mytypeworldcup.mytypeworldcup.domain.worldcup.entity.WorldCup;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface WorldCupMapper {
+
+    WorldCup worldCupPostDtoToWorldCup(WorldCupPostDto worldCupPostDto);
+
+    WorldCupResponseDto worldCupToWorldCupResponseDto(WorldCup worldCup);
+
+}
---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/repository/WorldCupRepository.java
@@ -0,0 +1,8 @@
+package com.mytypeworldcup.mytypeworldcup.domain.worldcup.repository;
+
+import com.mytypeworldcup.mytypeworldcup.domain.worldcup.entity.WorldCup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorldCupRepository extends JpaRepository<WorldCup, Long> {
+
+}
