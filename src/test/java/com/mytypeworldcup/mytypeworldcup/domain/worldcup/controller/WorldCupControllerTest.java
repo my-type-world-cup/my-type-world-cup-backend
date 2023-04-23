25c77a3f9a7a49d9b33eb25525bcf51b610e3c5d
Ksiyeong <yuiop2767@naver.com>
2023-04-23 21:33:39 +0900

    [test] getWorldCups 테스트

--- src/test/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/controller/WorldCupControllerTest.java
+++ src/test/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/controller/WorldCupControllerTest.java
@@ -1,37 +1,49 @@
 package com.mytypeworldcup.mytypeworldcup.domain.worldcup.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePostDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateResponseDto;
+import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateSimpleResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.service.CandidateService;
 import com.mytypeworldcup.mytypeworldcup.domain.member.service.MemberService;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupPostDto;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupResponseDto;
+import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupSimpleResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.service.WorldCupService;
+import com.mytypeworldcup.mytypeworldcup.global.common.PageResponseDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WorldCupController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureMockMvc // ???
 @WithMockUser(roles = {"USER", "ADMIN"})
+
 public class WorldCupControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -96,15 +108,15 @@ public class WorldCupControllerTest {
                 .memberId(memberId)
                 .build();
 
-        given(memberService.findMemberIdByEmail(Mockito.anyString())).willReturn(memberId);
-        given(worldCupService.createWorldCup(Mockito.any(WorldCupPostDto.class))).willReturn(response);
-        given(candidateService.createCandidates(Mockito.anyList())).willReturn(candidateResponseDtos);
+        given(memberService.findMemberIdByEmail(anyString())).willReturn(memberId);
+        given(worldCupService.createWorldCup(any(WorldCupPostDto.class))).willReturn(response);
+        given(candidateService.createCandidates(anyList())).willReturn(candidateResponseDtos);
 
         String content = gson.toJson(request);
 
         // when
         ResultActions actions = mockMvc.perform(
-                MockMvcRequestBuilders.post("/worldcups")
+                post("/worldcups")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .with(csrf())
@@ -120,8 +132,53 @@ public class WorldCupControllerTest {
                 .andExpect(jsonPath("$.visibility").value(request.getPassword() == null))
                 .andExpect(jsonPath("$.password").isEmpty())
                 .andExpect(jsonPath("$.memberId").value(memberId))
-                .andExpect(jsonPath("$.candidateResponseDtos").isArray())
+                .andExpect(jsonPath("$.candidateResponseDtos").isArray());
 //                .andExpect(jsonPath("$.candidateResponseDtos[*].worldCupId").isNumber())
-        ;
+
     }
+
+    @DisplayName("월드컵 가져오기 - 메인페이지")
+    @Test
+    void getWorldCupsTest() throws Exception {
+        // given
+        String page = "1";
+        String size = "5";
+        String sort = "playCount";
+        String direction = "DESC";
+        String keyword = "월드컵";
+
+        List<WorldCupSimpleResponseDto> data = new ArrayList<>();
+        for (long i = 1; i <= 3; i++) {
+            WorldCupSimpleResponseDto dummy = new WorldCupSimpleResponseDto(i, "월드컵 " + i, "설명입니다.");
+            dummy.setCandidateSimpleResponseDtos(List.of(
+                    new CandidateSimpleResponseDto(i, "테스트 이미지 " + i, "testURI"),
+                    new CandidateSimpleResponseDto(i + 3, "테스트 이미지 " + i + 3, "testURI")));
+            data.add(dummy);
+        }
+
+        Page<WorldCupSimpleResponseDto> responseDtos = new PageImpl<>(data);
+
+        given(worldCupService.searchWorldCups(any(Pageable.class), anyString())).willReturn(responseDtos);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/worldcups")
+                        .param("page", page)
+                        .param("size", size)
+                        .param("sort", sort)
+                        .param("direction", direction)
+                        .param("keyword", keyword)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk());
+
+        String actual = actions.andReturn().getResponse().getContentAsString();
+        String expected = gson.toJson(new PageResponseDto(responseDtos));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
