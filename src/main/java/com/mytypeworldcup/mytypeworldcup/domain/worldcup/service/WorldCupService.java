a856e670798ea36eb8c771184f75d34d9346f980
Ksiyeong <yuiop2767@naver.com>
2023-04-24 21:31:20 +0900

    [feat] 특정 월드컵 가져오기 메서드 구현
    findWorldCup, findVerifiedWorldCup 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/service/WorldCupService.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/service/WorldCupService.java
@@ -1,16 +1,17 @@
 package com.mytypeworldcup.mytypeworldcup.domain.worldcup.service;
 
-import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupMapper;
-import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupPostDto;
-import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupResponseDto;
-import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupSimpleResponseDto;
+import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.*;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.entity.WorldCup;
+import com.mytypeworldcup.mytypeworldcup.domain.worldcup.exception.WorldCupExceptionCode;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.repository.WorldCupRepository;
+import com.mytypeworldcup.mytypeworldcup.global.error.BusinessLogicException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -34,6 +35,18 @@ public class WorldCupService {
     @Transactional(readOnly = true)
     public Page<WorldCupSimpleResponseDto> searchWorldCups(Pageable pageable, String keyword) {
         return worldCupRepository.getWorldCupsWithCandidates(pageable, keyword);
+    }
+
+    @Transactional(readOnly = true)
+    public GetWorldCupResponseDto findWorldCup(long worldCupId) {
+        WorldCup worldCup = findVerifiedWorldCup(worldCupId);
+        return worldCupMapper.worldCupToGetWorldCupResponseDto(worldCup);
+    }
+
+    @Transactional(readOnly = true)
+    private WorldCup findVerifiedWorldCup(long worldCupId) {
+        Optional<WorldCup> optionalWorldCup = worldCupRepository.findById(worldCupId);
+        return optionalWorldCup.orElseThrow(() -> new BusinessLogicException(WorldCupExceptionCode.WORLD_CUP_NOT_FOUND));
     }
 
 }
