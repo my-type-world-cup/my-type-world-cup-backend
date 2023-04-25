80ceb1e2d740e89f1cceb5ed58d978f65df30cba
Ksiyeong <yuiop2767@naver.com>
2023-04-25 17:45:43 +0900

    [feat] postForGameStart 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/controller/WorldCupController.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/controller/WorldCupController.java
@@ -1,9 +1,11 @@
 package com.mytypeworldcup.mytypeworldcup.domain.worldcup.controller;
 
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateResponseDto;
+import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateSimpleResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.service.CandidateService;
 import com.mytypeworldcup.mytypeworldcup.domain.member.service.MemberService;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupPostDto;
+import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupRequestDto;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto.WorldCupSimpleResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.service.WorldCupService;
@@ -50,21 +52,20 @@ public class WorldCupController {
         return ResponseEntity.status(HttpStatus.CREATED).body(worldCupResponseDto);
     }
 
+    /**
+     * 파라미터 설명<p>
+     * page = 원하는 페이지 !!자체적으로 -1해서 계산함!!<p>
+     * size = 페이지당 볼 게시물 수<p>
+     * sort = playCount(인기순), createdAt(생성일순), commentCount(댓글순)<p>
+     * direction = DESC(내림차순), ASC(오름차순)<p>
+     * keyword = 검색어 (title, description 에서 검색)
+     */
     @GetMapping("/worldcups")
     public ResponseEntity getWorldCups(@Positive @RequestParam(required = false, defaultValue = "1") int page,
                                        @Positive @RequestParam(required = false, defaultValue = "5") int size,
                                        @RequestParam(required = false, defaultValue = "playCount") String sort,
                                        @RequestParam(required = false, defaultValue = "DESC") Sort.Direction direction,
                                        @RequestParam(required = false) String keyword) {
-        /**
-         * 파라미터 설명
-         * page = 원하는 페이지 !!자체적으로 -1해서 계산함!!
-         * size = 페이지당 볼 게시물 수
-         * sort = playCount(인기순), createdAt(생성일순), commentCount(댓글순)
-         * direction = DESC(내림차순), ASC(오름차순)
-         * keyword = 검색어 (title, description 에서 검색)
-         **/
-
         PageRequest pageRequest = PageRequest.of(page - 1, size, direction, sort);
         Page<WorldCupSimpleResponseDto> responseDtos = worldCupService.searchWorldCups(pageRequest, keyword);
 
@@ -76,4 +77,19 @@ public class WorldCupController {
         return ResponseEntity.ok(worldCupService.findWorldCup(worldCupId));
     }
 
+    /**
+     * 본격적인 월드컵 시작을 위해 월드컵에 사용될 Candidate들을 요청하는 메서드<p>
+     * 비밀번호를 입력받아야 하므로 POST 를 사용하였음
+     */
+    @PostMapping("/worldcups/candidates")
+    public ResponseEntity postForGameStart(@RequestBody WorldCupRequestDto worldCupRequestDto) {
+        Long worldCupId = worldCupRequestDto.getWorldCupId();
+        Integer teamCount = worldCupRequestDto.getTeamCount();
+        String password = worldCupRequestDto.getPassword();
+
+        worldCupService.verifyPassword(worldCupId, password);
+        List<CandidateSimpleResponseDto> candidateSimpleResponseDtos = candidateService.findCandidatesForGameStart(worldCupId, teamCount);
+
+        return ResponseEntity.ok(candidateSimpleResponseDtos);
+    }
 }
