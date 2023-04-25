8f186123076ce45081f4e5f7420bcf60f22c8ccf
Ksiyeong <yuiop2767@naver.com>
2023-04-25 13:54:44 +0900

    [refactor] jwt토큰 생성 위임
    CustomOAuth2UserService 토큰 생성로직 위임
    -> MemberAuthenticationSuccessHandler

--- src/main/java/com/mytypeworldcup/mytypeworldcup/global/auth/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/auth/oauth2/service/CustomOAuth2UserService.java
@@ -1,13 +1,9 @@
 package com.mytypeworldcup.mytypeworldcup.global.auth.oauth2.service;
 
 import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
-import com.mytypeworldcup.mytypeworldcup.domain.member.exception.MemberExceptionCode;
 import com.mytypeworldcup.mytypeworldcup.domain.member.repository.MemberRepository;
-import com.mytypeworldcup.mytypeworldcup.global.auth.jwt.JwtTokenizer;
 import com.mytypeworldcup.mytypeworldcup.global.auth.oauth2.dto.ProviderType;
-import com.mytypeworldcup.mytypeworldcup.global.error.BusinessLogicException;
 import com.mytypeworldcup.mytypeworldcup.global.util.CustomAuthorityUtils;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -16,8 +12,6 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Collections;
 import java.util.Map;
@@ -28,7 +22,6 @@ import java.util.Optional;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final MemberRepository memberRepository;
     private final CustomAuthorityUtils authorityUtils;
-    private final JwtTokenizer jwtTokenizer;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -67,30 +60,25 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         Member member;
 
         if (optionalUser.isPresent()) { // 이미 있는 사람이면
-            member = optionalUser.get();
-            if (member.getProviderType() == ProviderType.NATIVE) {
-                throw new BusinessLogicException(MemberExceptionCode.MEMBER_EXISTS); // Todo 네이티브로 회원가입한 회원이라고 알려주자
-                // Todo 에러 발생시 GlobalExceptionAdvice가 못잡음 500에러 발생함 시발
-            }
+            member = optionalUser.get(); // member를 돌려준다.
+
+//            if (member.getProviderType() == ProviderType.NATIVE) { // 가입한적은 있으나 네이티브로 가입했을 경우 에러발생
+//                throw new BusinessLogicException(MemberExceptionCode.MEMBER_EXISTS); // Todo 네이티브로 회원가입한 회원이라고 알려주자
+            // Todo 에러 발생시 GlobalExceptionAdvice가 못잡음 500에러 발생함
+//            } 네이티브 로그인 회원가입 시 이메일 인증을 하도록하면 굳이 안해도 된다 ???????
+
+
         } else { // 가입한적 없는 사람이면
             member = Member.builder()
                     .email(email)
                     .nickname(name)
                     .password("oauth") //TODO 방법 생각해내라
                     .providerType(providerType)
-                    .roles(authorityUtils.createRoles(email)) //TODO 관련 로직 만드세요!!
+                    .roles(authorityUtils.createRoles(email))
                     .build();
 
             memberRepository.save(member);
         }
-
-        String accessToken = jwtTokenizer.delegateAccessToken(member);
-        String refreshToken = jwtTokenizer.delegateRefreshToken(member);
-
-        HttpServletResponse response2 = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
-        response2.addHeader("Authorization", "Bearer " + accessToken);
-        response2.addHeader("Refresh", refreshToken);
-
 
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority(member.getRoles().toString())),
