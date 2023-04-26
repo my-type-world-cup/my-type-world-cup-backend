616b2a025686eb0ad9f2070e44516322f940402c
Ksiyeong <yuiop2767@naver.com>
2023-04-26 12:34:41 +0900

    [feat] CandidateController.patchMatchResults 메서드 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/controller/CandidateController.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/controller/CandidateController.java
@@ -1,13 +1,12 @@
 package com.mytypeworldcup.mytypeworldcup.domain.candidate.controller;
 
+import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePatchDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.service.CandidateService;
 import com.mytypeworldcup.mytypeworldcup.global.util.ImageCrawler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +22,15 @@ public class CandidateController {
         List<String> imageUrls = imageCrawler.getImageUrls(keyword);
         return ResponseEntity.ok(imageUrls);
     }
+
+    /**
+     * 경기 결과를 반영하는 메서드<p>
+     * 결과를 리스트로 받는다
+     */
+    @PatchMapping("/candidates")
+    public ResponseEntity patchMatchResults(@RequestBody List<CandidatePatchDto> candidatePatchDtos) {
+        candidateService.updateMatchResults(candidatePatchDtos);
+        return ResponseEntity.ok("업데이트가 정상적으로 처리되었습니다.");
+    }
+
 }
