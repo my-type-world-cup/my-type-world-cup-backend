9f290542c5d87573e03fb12c8bff778048f9f1eb
Ksiyeong <yuiop2767@naver.com>
2023-04-26 17:05:34 +0900

    [feat] CandidateRepository.findAllByWorldCup_Id() 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/repository/CandidateRepository.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/repository/CandidateRepository.java
@@ -3,5 +3,8 @@ package com.mytypeworldcup.mytypeworldcup.domain.candidate.repository;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CandidateRepository extends JpaRepository<Candidate, Long>, CandidateRepositoryCustom {
+    List<Candidate> findAllByWorldCup_Id(Long worldCupId);
 }
