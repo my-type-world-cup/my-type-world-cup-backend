9637863f2b63c4a1849183c043639193b43a604e
Ksiyeong <yuiop2767@naver.com>
2023-05-01 22:44:37 +0900

    [feat] NaverItemResponseDto 추가

---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/common/NaverItemResponseDto.java
@@ -0,0 +1,12 @@
+package com.mytypeworldcup.mytypeworldcup.global.common;
+
+import lombok.Getter;
+
+@Getter
+public class NaverItemResponseDto {
+    private String title;
+    private String link;
+    private String thumbnail;
+    private String sizeheight;
+    private String sizewidth;
+}
