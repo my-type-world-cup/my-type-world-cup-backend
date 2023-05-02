0fd05665467a21b6dea80789cea89f25e90de572
Ksiyeong <yuiop2767@naver.com>
2023-05-02 21:54:36 +0900

    [feat] LikeExceptionCode 404, 403 에러코드 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/exception/LikeExceptionCode.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/exception/LikeExceptionCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum LikeExceptionCode implements ExceptionCode {
-    LIKE_EXISTS(409, "해당 댓글에 이미 좋아요를 눌렀습니다.");
+    LIKE_EXISTS(409, "해당 댓글에 이미 좋아요를 눌렀습니다."),
+    LIKE_NOT_FOUND(404, "좋아요를 찾을 수 없습니다."),
+    FORBIDDEN(403, "자신이 누른 좋아요만 취소할 수 있습니다.");
     private int status;
     private String message;
 }
