d9bfadade9455255654c0eb0005a495e85f3763a
Ksiyeong <yuiop2767@naver.com>
2023-05-02 21:54:09 +0900

    [feat] LikeController.deleteLike() 추가
    좋아요 취소 기능 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/controller/LikeController.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/controller/LikeController.java
@@ -4,13 +4,12 @@ import com.mytypeworldcup.mytypeworldcup.domain.like.dto.LikePostDto;
 import com.mytypeworldcup.mytypeworldcup.domain.like.service.LikeService;
 import com.mytypeworldcup.mytypeworldcup.domain.member.service.MemberService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,5 +34,13 @@ public class LikeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @DeleteMapping("/likes/{likeId}")
+    public ResponseEntity deleteLike(Authentication authentication,
+                                     @Positive @PathVariable Long likeId) {
 
+        Long memberId = memberService.findMemberIdByEmail(authentication.getName());
+        likeService.deleteLike(memberId, likeId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
