a9ff5f628ab15cc7dd9c119a9628ddb917762426
Ksiyeong <yuiop2767@naver.com>
2023-05-08 19:42:00 +0900

    [test] requestCandidatesByWorldCupId() 테스트
    CandidateController.requestCandidatesByWorldCupId
    테스트 작성

--- src/test/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/controller/CandidateControllerTest.java
+++ src/test/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/controller/CandidateControllerTest.java
@@ -3,16 +3,22 @@ package com.mytypeworldcup.mytypeworldcup.domain.candidate.controller;
 import com.google.gson.Gson;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePatchDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateRequestDto;
+import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateSimpleResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.service.CandidateService;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.service.WorldCupService;
+import com.mytypeworldcup.mytypeworldcup.global.common.PageResponseDto;
 import com.mytypeworldcup.mytypeworldcup.global.util.NaverSearchAPI;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -21,8 +27,9 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.mockito.BDDMockito.given;
+
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -123,6 +130,65 @@ class CandidateControllerTest {
     }
 
     @Test
-    void requestCandidatesByWorldCupId() {
+    @DisplayName("후보목록 가져오기")
+    void requestCandidatesByWorldCupId() throws Exception {
+        // given
+        int page = 1;
+        int size = 5;
+        String sort = "winCount";
+        String direction = "DESC";
+        String keyword = null;
+
+        CandidateRequestDto request = CandidateRequestDto
+                .builder()
+                .worldCupId(1L)
+                .password(null)
+                .build();
+
+        List<CandidateResponseDto> data = new ArrayList<>();
+        for (long i = 1; i <= 10; i++) {
+            CandidateResponseDto candidateResponseDto = CandidateResponseDto
+                    .builder()
+                    .id(i)
+                    .name("테스트후보명" + i)
+                    .image("테스트이미지url" + i)
+                    .finalWinCount((int) (10L - i))
+                    .winCount((int) (10L - i))
+                    .matchUpWorldCupCount(100)
+                    .matchUpGameCount(100)
+                    .worldCupId(request.getWorldCupId())
+                    .build();
+            data.add(candidateResponseDto);
+        }
+
+        Page<CandidateResponseDto> responseDtos = new PageImpl(data);
+
+        doNothing().when(worldCupService).verifyPassword(anyLong(), anyString());
+        given(candidateService.findCandidatesByWorldCupId(anyLong(), isNull(), any(Pageable.class))).willReturn(responseDtos);
+
+        String content = gson.toJson(request);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                post("/candidates/search")
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size))
+                        .param("sort", sort)
+                        .param("direction", direction)
+                        .param("keyword", keyword)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf())
+                        .content(content)
+        );
+
+        // then
+        actions
+                .andExpect(status().isOk());
+
+        String actual = actions.andReturn().getResponse().getContentAsString();
+        String expected = gson.toJson(new PageResponseDto(responseDtos));
+
+        Assertions.assertEquals(expected, actual);
     }
 }
