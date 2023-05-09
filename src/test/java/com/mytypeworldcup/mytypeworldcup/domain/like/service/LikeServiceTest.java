b79ff4e525a50af43d8f65800f8557306bbdc90a
Ksiyeong <yuiop2767@naver.com>
2023-05-09 14:08:40 +0900

    [test] LikeServiceTest 작성
    deleteLike() 해피케이스, 권한없음, 좋아요 없음 테스트

--- src/test/java/com/mytypeworldcup/mytypeworldcup/domain/like/service/LikeServiceTest.java
+++ src/test/java/com/mytypeworldcup/mytypeworldcup/domain/like/service/LikeServiceTest.java
@@ -5,6 +5,7 @@ import com.mytypeworldcup.mytypeworldcup.domain.like.dto.LikePostDto;
 import com.mytypeworldcup.mytypeworldcup.domain.like.entity.Like;
 import com.mytypeworldcup.mytypeworldcup.domain.like.exception.LikeExceptionCode;
 import com.mytypeworldcup.mytypeworldcup.domain.like.repository.LikeRepository;
+import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
 import com.mytypeworldcup.mytypeworldcup.global.error.BusinessLogicException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,11 +17,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.lang.reflect.Field;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LikeServiceTest {
@@ -85,6 +84,66 @@ class LikeServiceTest {
     }
 
     @Test
-    void deleteLike() {
+    @DisplayName("좋아요 삭제 - 정상통과")
+    void deleteLike_happy() {
+        // given
+        Long memberId = 3L;
+        Long likeId = 1L;
+
+        Member member = new Member();
+        member.setId(memberId);
+
+        Like like = Like
+                .builder()
+                .member(member)
+                .build();
+
+        given(likeRepository.findById(anyLong())).willReturn(Optional.ofNullable(like));
+
+        // when
+        assertDoesNotThrow(() -> likeService.deleteLike(memberId, likeId));
+
+        // then
+        verify(likeRepository).delete(like);
+    }
+
+    @Test
+    @DisplayName("좋아요 삭제 - 권한없음")
+    void deleteLike_FORBIDDEN() {
+        // given
+        Long memberId = 1L;
+        Long likeId = 10L;
+
+        Member member = new Member();
+        member.setId(2L); // 다른 멤버의 ID
+
+        Like like = Like
+                .builder()
+                .member(member)
+                .build();
+
+        given(likeRepository.findById(likeId)).willReturn(Optional.ofNullable(like));
+
+        // when
+        // then
+        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.deleteLike(memberId, likeId));
+        assertEquals(LikeExceptionCode.FORBIDDEN, thrown.getExceptionCode());
+        verify(likeRepository, never()).delete(like);
+    }
+
+    @Test
+    @DisplayName("좋아요 삭제 - 좋아요 없음")
+    void deleteLike_LIKE_NOT_FOUND() {
+        // given
+        Long memberId = 1L;
+        Long likeId = 10L;
+
+        given(likeRepository.findById(likeId)).willReturn(Optional.ofNullable(null));
+
+        // when
+        // then
+        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.deleteLike(memberId, likeId));
+        assertEquals(LikeExceptionCode.LIKE_NOT_FOUND, thrown.getExceptionCode());
+        verify(likeRepository, never()).delete(any(Like.class));
     }
 }
