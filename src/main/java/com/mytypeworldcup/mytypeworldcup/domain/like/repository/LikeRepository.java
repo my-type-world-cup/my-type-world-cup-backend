1dac28bde59b92ba711f796a2f5d22283e77505e
Ksiyeong <yuiop2767@naver.com>
2023-05-14 15:57:08 +0900

    [feat] LikeRepository.findByMemberAndComment() 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/repository/LikeRepository.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/repository/LikeRepository.java
@@ -1,10 +1,12 @@
 package com.mytypeworldcup.mytypeworldcup.domain.like.repository;
 
+import com.mytypeworldcup.mytypeworldcup.domain.comment.entity.Comment;
 import com.mytypeworldcup.mytypeworldcup.domain.like.entity.Like;
+import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    Optional<Like> findByComment_IdAndMember_Id(Long commentId, Long memberId);
+    Optional<Like> findByMemberAndComment(Member member, Comment comment);
 }
