8a69acb138ad0f97141859a60dd3ed0162bc451a
Ksiyeong <yuiop2767@naver.com>
2023-05-14 15:54:53 +0900

    [refactor] LikeService 로직수정 및 테스트 수정

--- src/test/java/com/mytypeworldcup/mytypeworldcup/domain/like/service/LikeServiceTest.java
+++ src/test/java/com/mytypeworldcup/mytypeworldcup/domain/like/service/LikeServiceTest.java
@@ -1,7 +1,6 @@
 package com.mytypeworldcup.mytypeworldcup.domain.like.service;
 
-import com.mytypeworldcup.mytypeworldcup.domain.like.dto.LikeMapper;
-import com.mytypeworldcup.mytypeworldcup.domain.like.dto.LikePostDto;
+import com.mytypeworldcup.mytypeworldcup.domain.comment.entity.Comment;
 import com.mytypeworldcup.mytypeworldcup.domain.like.entity.Like;
 import com.mytypeworldcup.mytypeworldcup.domain.like.exception.LikeExceptionCode;
 import com.mytypeworldcup.mytypeworldcup.domain.like.repository.LikeRepository;
@@ -14,7 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.lang.reflect.Field;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,59 +25,45 @@ class LikeServiceTest {
     private LikeService likeService;
     @Mock
     private LikeRepository likeRepository;
-    @Mock
-    private LikeMapper likeMapper;
-
 
     @Test
     @DisplayName("좋아요 생성 - 정상통과")
-    void createLike_happy() throws IllegalAccessException, NoSuchFieldException {
+    void createLike_happy() {
         // given
-        LikePostDto likePostDto = LikePostDto
-                .builder()
-                .commentId(1L)
-                .build();
-        likePostDto.setMemberId(3L);
+        Member member = new Member();
+        member.setId(1L);
 
-        Like like = Like
-                .builder()
-                .comment(likePostDto.getComment())
-                .member(likePostDto.getMember())
-                .build();
+        Comment comment = new Comment();
+        comment.setId(2L);
 
-        // Like id 임시로 변경
-        Field idField = like.getClass().getDeclaredField("id");
-        idField.setAccessible(true);
-        idField.set(like, 1L);
+        Like like = Like.builder().member(member).comment(comment).build();
 
-        given(likeRepository.findByComment_IdAndMember_Id(anyLong(), anyLong())).willReturn(Optional.ofNullable(null));
-        given(likeMapper.likePostDtoToLike(any(LikePostDto.class))).willReturn(like);
+        given(likeRepository.findByMemberAndComment(any(Member.class), any(Comment.class))).willReturn(Optional.ofNullable(null));
         given(likeRepository.save(any(Like.class))).willReturn(like);
 
         // when
-        Long actual = likeService.createLike(likePostDto);
+        likeService.createLike(member, comment);
 
         // then
-        assertEquals(1L, actual);
-        assertEquals(likePostDto.getCommentId(), like.getComment().getId());
-        assertEquals(likePostDto.getMemberId(), like.getMember().getId());
+        verify(likeRepository, times(1)).save(any(Like.class));
     }
 
     @Test
     @DisplayName("좋아요 생성 - 이미좋아요를 누른경우")
     void createLike_bad() {
-        // given
-        LikePostDto likePostDto = LikePostDto
-                .builder()
-                .commentId(1L)
-                .build();
-        likePostDto.setMemberId(3L);
+        Member member = new Member();
+        member.setId(1L);
 
-        given(likeRepository.findByComment_IdAndMember_Id(anyLong(), anyLong())).willReturn(Optional.ofNullable(new Like()));
+        Comment comment = new Comment();
+        comment.setId(2L);
+
+        Like like = Like.builder().member(member).comment(comment).build();
+
+        given(likeRepository.findByMemberAndComment(any(Member.class), any(Comment.class))).willReturn(Optional.ofNullable(like));
 
         // when
         // then
-        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.createLike(likePostDto));
+        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.createLike(member, comment));
         assertEquals(LikeExceptionCode.LIKE_EXISTS, thrown.getExceptionCode());
     }
 
@@ -87,62 +71,38 @@ class LikeServiceTest {
     @DisplayName("좋아요 삭제 - 정상통과")
     void deleteLike_happy() {
         // given
-        Long memberId = 3L;
-        Long likeId = 1L;
-
         Member member = new Member();
-        member.setId(memberId);
+        member.setId(1L);
 
-        Like like = Like
-                .builder()
-                .member(member)
-                .build();
+        Comment comment = new Comment();
+        comment.setId(2L);
 
-        given(likeRepository.findById(anyLong())).willReturn(Optional.ofNullable(like));
+        Like like = Like.builder().member(member).comment(comment).build();
+
+        given(likeRepository.findByMemberAndComment(any(Member.class), any(Comment.class))).willReturn(Optional.ofNullable(like));
 
         // when
-        assertDoesNotThrow(() -> likeService.deleteLike(memberId, likeId));
+        assertDoesNotThrow(() -> likeService.deleteLike(member, comment));
 
         // then
         verify(likeRepository).delete(like);
     }
 
     @Test
-    @DisplayName("좋아요 삭제 - 권한없음")
-    void deleteLike_FORBIDDEN() {
-        // given
-        Long memberId = 1L;
-        Long likeId = 10L;
-
-        Member member = new Member();
-        member.setId(2L); // 다른 멤버의 ID
-
-        Like like = Like
-                .builder()
-                .member(member)
-                .build();
-
-        given(likeRepository.findById(likeId)).willReturn(Optional.ofNullable(like));
-
-        // when
-        // then
-        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.deleteLike(memberId, likeId));
-        assertEquals(LikeExceptionCode.FORBIDDEN, thrown.getExceptionCode());
-        verify(likeRepository, never()).delete(like);
-    }
-
-    @Test
     @DisplayName("좋아요 삭제 - 좋아요 없음")
     void deleteLike_LIKE_NOT_FOUND() {
         // given
-        Long memberId = 1L;
-        Long likeId = 10L;
+        Member member = new Member();
+        member.setId(1L);
 
-        given(likeRepository.findById(likeId)).willReturn(Optional.ofNullable(null));
+        Comment comment = new Comment();
+        comment.setId(2L);
+
+        given(likeRepository.findByMemberAndComment(any(Member.class), any(Comment.class))).willReturn(Optional.ofNullable(null));
 
         // when
         // then
-        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.deleteLike(memberId, likeId));
+        BusinessLogicException thrown = assertThrows(BusinessLogicException.class, () -> likeService.deleteLike(member, comment));
         assertEquals(LikeExceptionCode.LIKE_NOT_FOUND, thrown.getExceptionCode());
         verify(likeRepository, never()).delete(any(Like.class));
     }
