0a3bea9b1c0a4918db06d0461bbadd638cde4a67
Ksiyeong <yuiop2767@naver.com>
2023-05-14 15:54:28 +0900

    [refactor] LikeController url 및 로직 수정

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/controller/LikeController.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/controller/LikeController.java
@@ -1,44 +1,47 @@
 package com.mytypeworldcup.mytypeworldcup.domain.like.controller;
 
-import com.mytypeworldcup.mytypeworldcup.domain.like.dto.LikePostDto;
+import com.mytypeworldcup.mytypeworldcup.domain.comment.entity.Comment;
+import com.mytypeworldcup.mytypeworldcup.domain.comment.service.CommentService;
 import com.mytypeworldcup.mytypeworldcup.domain.like.service.LikeService;
+import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
 import com.mytypeworldcup.mytypeworldcup.domain.member.service.MemberService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-
-import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class LikeController {
     private final MemberService memberService;
+    private final CommentService commentService;
     private final LikeService likeService;
 
-    @PostMapping("/likes")
+    @PostMapping("/comments/{commentId}/likes")
     public ResponseEntity postLike(Authentication authentication,
-                                   @Valid @RequestBody LikePostDto likePostDto) {
-        // 멤버아이디 세팅
-        Long memberId = memberService.findMemberIdByEmail(authentication.getName());
-        likePostDto.setMemberId(memberId);
+                                   @Positive @PathVariable Long commentId) {
 
-        Long likeId = likeService.createLike(likePostDto);
-        URI location = fromCurrentRequest().path("/{id}").buildAndExpand(likeId).toUri();
+        Member member = memberService.findVerifiedMemberByEmail(authentication.getName());
+        Comment comment = commentService.findVerifiedCommentById(commentId);
 
-        return ResponseEntity.created(location).build();
+        likeService.createLike(member, comment);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping("/likes/{likeId}")
+    @DeleteMapping("/comments/{commentId}/likes")
     public ResponseEntity deleteLike(Authentication authentication,
-                                     @Positive @PathVariable Long likeId) {
+                                     @Positive @PathVariable Long commentId) {
 
-        Long memberId = memberService.findMemberIdByEmail(authentication.getName());
-        likeService.deleteLike(memberId, likeId);
+        Member member = memberService.findVerifiedMemberByEmail(authentication.getName());
+        Comment comment = commentService.findVerifiedCommentById(commentId);
+
+        likeService.deleteLike(member, comment);
 
         return ResponseEntity.noContent().build();
     }
--- src/test/java/com/mytypeworldcup/mytypeworldcup/domain/like/controller/LikeControllerTest.java
+++ src/test/java/com/mytypeworldcup/mytypeworldcup/domain/like/controller/LikeControllerTest.java
@@ -1,8 +1,9 @@
 package com.mytypeworldcup.mytypeworldcup.domain.like.controller;
 
-import com.google.gson.Gson;
-import com.mytypeworldcup.mytypeworldcup.domain.like.dto.LikePostDto;
+import com.mytypeworldcup.mytypeworldcup.domain.comment.entity.Comment;
+import com.mytypeworldcup.mytypeworldcup.domain.comment.service.CommentService;
 import com.mytypeworldcup.mytypeworldcup.domain.like.service.LikeService;
+import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
 import com.mytypeworldcup.mytypeworldcup.domain.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,7 +21,6 @@ import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LikeController.class)
@@ -30,11 +29,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class LikeControllerTest {
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private Gson gson;
     @MockBean
     private MemberService memberService;
 
+    @MockBean
+    private CommentService commentService;
     @MockBean
     private LikeService likeService;
 
@@ -43,46 +42,36 @@ class LikeControllerTest {
     void postLike() throws Exception {
         // given
         Long commentId = 1L;
-        Long memberId = 4L;
-        Long likeId = 10L;
 
-        LikePostDto request = LikePostDto
-                .builder()
-                .commentId(commentId)
-                .build();
-
-        given(memberService.findMemberIdByEmail(anyString())).willReturn(memberId);
-        given(likeService.createLike(any(LikePostDto.class))).willReturn(likeId);
-
-        String content = gson.toJson(request);
+        given(memberService.findVerifiedMemberByEmail(anyString())).willReturn(new Member());
+        given(commentService.findVerifiedCommentById(anyLong())).willReturn(new Comment());
+        doNothing().when(likeService).createLike(any(Member.class), any(Comment.class));
 
         // when
         ResultActions actions = mockMvc.perform(
-                post("/likes")
+                post("/comments/{commentId}/likes", commentId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .with(csrf())
-                        .content(content)
         );
 
         // then
         actions
-                .andExpect(status().isCreated())
-                .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost/likes/" + likeId));
+                .andExpect(status().isCreated());
     }
 
     @Test
     @DisplayName("좋아요 취소")
     void deleteLike() throws Exception {
         // given
-        Long likeId = 10L;
-        Long memberId = 4L;
+        Long commentId = 1L;
 
-        given(memberService.findMemberIdByEmail(anyString())).willReturn(memberId);
-        doNothing().when(likeService).deleteLike(anyLong(), anyLong());
+        given(memberService.findVerifiedMemberByEmail(anyString())).willReturn(new Member());
+        given(commentService.findVerifiedCommentById(anyLong())).willReturn(new Comment());
+        doNothing().when(likeService).deleteLike(any(Member.class), any(Comment.class));
 
         // when
         ResultActions actions = mockMvc.perform(
-                delete("/likes/{likeId}", likeId)
+                delete("/comments/{commentId}/likes", commentId)
                         .with(csrf())
         );
 
