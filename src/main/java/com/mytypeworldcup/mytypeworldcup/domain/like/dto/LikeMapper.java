17704584ef7e1117904b13d65be1a2f87a5660fd
Ksiyeong <yuiop2767@naver.com>
2023-05-14 15:44:59 +0900

    [remove] LikeMapper 삭제
    LikeMapper 미사용으로 인해 삭제
    LikeMapperTest 삭제

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/dto/LikeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mytypeworldcup.mytypeworldcup.domain.like.dto;
-
-import com.mytypeworldcup.mytypeworldcup.domain.like.entity.Like;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface LikeMapper {
-    Like likePostDtoToLike(LikePostDto likePostDto);
-}
