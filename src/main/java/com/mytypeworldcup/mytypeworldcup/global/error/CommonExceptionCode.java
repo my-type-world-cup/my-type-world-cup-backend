d4b346fdcd03e51af89e8e187344594f9764d94b
Ksiyeong <yuiop2767@naver.com>
2023-05-15 16:43:59 +0900

    [feat] 400번 에러 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/global/error/CommonExceptionCode.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/error/CommonExceptionCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CommonExceptionCode implements ExceptionCode {
     INVALID_PASSWORD(401, "비밀번호가 유효하지 않습니다."),
-    SERVICE_UNAVAILABLE(503, "서버에서 일시적으로 요청을 처리할 수 없습니다. 잠시 후 다시 시도해주세요.");
+    SERVICE_UNAVAILABLE(503, "서버에서 일시적으로 요청을 처리할 수 없습니다. 잠시 후 다시 시도해주세요."),
+    BAD_REQUEST(400, "잘못된 요청입니다.");
     private int status;
     private String message;
 }
