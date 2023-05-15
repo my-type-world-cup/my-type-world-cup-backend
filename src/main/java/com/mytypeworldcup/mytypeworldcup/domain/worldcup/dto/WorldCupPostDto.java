3771efb90d2f3a4b094e36f5cf1c17d6e33514cc
Ksiyeong <yuiop2767@naver.com>
2023-05-15 17:37:48 +0900

    [refactor] WorldCupPostDto 유효성 검사추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/dto/WorldCupPostDto.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/dto/WorldCupPostDto.java
@@ -2,6 +2,7 @@ package com.mytypeworldcup.mytypeworldcup.domain.worldcup.dto;
 
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePostDto;
 import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -30,6 +31,7 @@ public class WorldCupPostDto {
     private String password;
 
     @NotNull
+    @Valid
     private List<CandidatePostDto> candidatePostDtos;
 
     @Builder
