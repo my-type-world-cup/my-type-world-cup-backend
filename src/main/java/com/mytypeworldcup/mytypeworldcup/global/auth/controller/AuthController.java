d9bac902b14bdb134a35ff832b6e5fe849403967
Ksiyeong <yuiop2767@naver.com>
2023-05-18 15:30:35 +0900

    [feat] AuthController 추가

---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.mytypeworldcup.mytypeworldcup.global.auth.controller;
+
+import com.mytypeworldcup.mytypeworldcup.global.auth.service.RefreshService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final RefreshService refreshService;
+
+    @PostMapping("/auth/refresh")
+    public ResponseEntity getAccessTokenByRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+        refreshService.refreshAccessToken(request, response);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/auth/logout")
+    public ResponseEntity logout(Authentication authentication) {
+        refreshService.logout(authentication.getName());
+        return ResponseEntity.noContent().build();
+    }
+
+}
