75dd4096f9e4a977858fe9edb2a254cc7885cbaf
Ksiyeong <yuiop2767@naver.com>
2023-05-21 16:37:06 +0900

    [feat] CandidateController.postCandidate() 추가
    후보 등록 메서드 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/controller/CandidateController.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/controller/CandidateController.java
@@ -1,19 +1,22 @@
 package com.mytypeworldcup.mytypeworldcup.domain.candidate.controller;
 
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePatchDto;
+import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidatePostDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.dto.CandidateSimpleResponseDto;
-import com.mytypeworldcup.mytypeworldcup.global.common.PasswordDto;
 import com.mytypeworldcup.mytypeworldcup.domain.candidate.service.CandidateService;
 import com.mytypeworldcup.mytypeworldcup.domain.worldcup.service.WorldCupService;
 import com.mytypeworldcup.mytypeworldcup.global.common.PageResponseDto;
+import com.mytypeworldcup.mytypeworldcup.global.common.PasswordDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +30,14 @@ import java.util.Map;
 public class CandidateController {
     private final WorldCupService worldCupService;
     private final CandidateService candidateService;
+
+    @PostMapping("/candidates")
+    public ResponseEntity postCandidate(Authentication authentication,
+                                        @RequestBody CandidatePostDto candidatePostDto) {
+        worldCupService.findWorldCupDetails(authentication.getName(), candidatePostDto.getWorldCupId());
+        CandidateResponseDto candidateResponseDto = candidateService.createCandidate(candidatePostDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(candidateResponseDto);
+    }
 
     /**
      * 경기 결과를 반영하는 메서드<p>
