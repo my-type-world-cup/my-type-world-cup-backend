a6a188556e9f022d901fe900fbd485a9996b8409
Ksiyeong <yuiop2767@naver.com>
2023-05-21 18:54:00 +0900

    [feat] 권한증명, 업데이트메서드 추가
    verifyWorldCupAccess() 수정권한 증명 메서드 추가,
    updateWorldCup() WorldCup 업데이트 메서드 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/service/WorldCupService.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/worldcup/service/WorldCupService.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -31,6 +33,22 @@ public class WorldCupService {
         return worldCupMapper.worldCupToWorldCupResponseDto(savedWorldCup);
     }
 
+    public WorldCupResponseDto updateWorldCup(long worldCupId, WorldCupPatchDto worldCupPatchDto) {
+        WorldCup worldCup = findVerifiedWorldCup(worldCupId);
+
+        Optional.ofNullable(worldCupPatchDto.getTitle()).ifPresent(data -> worldCup.updateTitle(data));
+        Optional.ofNullable(worldCupPatchDto.getDescription()).ifPresent(data -> worldCup.updateDescription(data));
+        Optional.ofNullable(worldCupPatchDto.getPassword()).ifPresent(data -> {
+            if (data.equals("null")) {
+                worldCup.updatePassword(null);
+            } else {
+                worldCup.updatePassword(data);
+            }
+        });
+
+        return worldCupMapper.worldCupToWorldCupResponseDto(worldCup);
+    }
+
     @Transactional(readOnly = true)
     public Page<WorldCupSimpleResponseDto> searchWorldCups(Long memberId, String keyword, Pageable pageable) {
         return worldCupRepository.getWorldCupsWithCandidates(memberId, keyword, pageable);
@@ -43,11 +61,8 @@ public class WorldCupService {
     }
 
     @Transactional(readOnly = true)
-    public WorldCupResponseDto findWorldCupDetails(String email, long worldCupId) {
+    public WorldCupResponseDto findWorldCupDetails(long worldCupId) {
         WorldCup worldCup = findVerifiedWorldCup(worldCupId);
-        if (!worldCup.getMember().getEmail().equals(email)) {
-            throw new BusinessLogicException(CommonExceptionCode.FORBIDDEN);
-        }
         return worldCupMapper.worldCupToWorldCupResponseDto(worldCup);
     }
 
@@ -56,6 +71,14 @@ public class WorldCupService {
         String worldCupPassword = findVerifiedWorldCup(worldCupId).getPassword();
         if (worldCupPassword != null && !worldCupPassword.equals(password)) {
             throw new BusinessLogicException(CommonExceptionCode.INVALID_PASSWORD);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public void verifyWorldCupAccess(String email, long worldCupId) {
+        WorldCup worldCup = findVerifiedWorldCup(worldCupId);
+        if (!worldCup.getMember().getEmail().equals(email)) {
+            throw new BusinessLogicException(CommonExceptionCode.FORBIDDEN);
         }
     }
 
