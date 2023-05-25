2dd683e65889e044dfd3b6be6775f0a744338294
Ksiyeong <yuiop2767@naver.com>
2023-05-25 16:42:04 +0900

    [feat] ExpiredJwtException 예외 처리

--- src/main/java/com/mytypeworldcup/mytypeworldcup/global/error/GlobalExceptionAdvice.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/error/GlobalExceptionAdvice.java
@@ -1,5 +1,8 @@
 package com.mytypeworldcup.mytypeworldcup.global.error;
 
+import com.mytypeworldcup.mytypeworldcup.global.auth.exception.AuthExceptionCode;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,4 +15,11 @@ public class GlobalExceptionAdvice {
         final ErrorResponse errorResponse = ErrorResponse.of(e.getExceptionCode());
         return new ResponseEntity(errorResponse, HttpStatusCode.valueOf(e.getExceptionCode().getStatus()));
     }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> handle(ExpiredJwtException ex) {
+        final ErrorResponse errorResponse = ErrorResponse.of(AuthExceptionCode.REFRESH_TOKEN_EXPIRED);
+        return ResponseEntity.status(AuthExceptionCode.REFRESH_TOKEN_EXPIRED.getStatus()).body(errorResponse);
+    }
+
 }
