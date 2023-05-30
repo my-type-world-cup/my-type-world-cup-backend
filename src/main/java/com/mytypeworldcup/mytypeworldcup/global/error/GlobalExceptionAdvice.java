b97ddc297d8cc757441b5f6a88d5d7d605addbf8
Ksiyeong <yuiop2767@naver.com>
2023-05-30 22:58:28 +0900

    [refactor] handle 메서드명 명확하게 변경

--- src/main/java/com/mytypeworldcup/mytypeworldcup/global/error/GlobalExceptionAdvice.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/error/GlobalExceptionAdvice.java
@@ -2,8 +2,6 @@ package com.mytypeworldcup.mytypeworldcup.global.error;
 
 import com.mytypeworldcup.mytypeworldcup.global.auth.exception.AuthExceptionCode;
 import io.jsonwebtoken.ExpiredJwtException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,13 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
     @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity<ErrorResponse> handle(BusinessLogicException e) {
+    public ResponseEntity<ErrorResponse> handleBusinessLogicException(BusinessLogicException e) {
         final ErrorResponse errorResponse = ErrorResponse.of(e.getExceptionCode());
-        return new ResponseEntity(errorResponse, HttpStatusCode.valueOf(e.getExceptionCode().getStatus()));
+        return ResponseEntity.status(e.getExceptionCode().getStatus()).body(errorResponse);
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<ErrorResponse> handle(ExpiredJwtException ex) {
+    public ResponseEntity<ErrorResponse> handleExpiredJwtException(ExpiredJwtException ex) {
         final ErrorResponse errorResponse = ErrorResponse.of(AuthExceptionCode.REFRESH_TOKEN_EXPIRED);
         return ResponseEntity.status(AuthExceptionCode.REFRESH_TOKEN_EXPIRED.getStatus()).body(errorResponse);
     }
