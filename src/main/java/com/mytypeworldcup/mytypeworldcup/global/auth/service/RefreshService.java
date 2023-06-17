0ae84474565d0ccb3c2cf732db7ef6b88497a334
Ksiyeong <yuiop2767@naver.com>
2023-06-17 13:55:09 +0900

    [feat] 리프레쉬서비스 추가

--- src/main/java/com/mytypeworldcup/mytypeworldcup/global/auth/service/RefreshService.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/global/auth/service/RefreshService.java
@@ -8,7 +8,9 @@ import com.mytypeworldcup.mytypeworldcup.global.auth.repository.RefreshTokenRepo
 import com.mytypeworldcup.mytypeworldcup.global.auth.utils.CookieUtil;
 import com.mytypeworldcup.mytypeworldcup.global.auth.utils.HeaderUtil;
 import com.mytypeworldcup.mytypeworldcup.global.error.BusinessLogicException;
+import com.mytypeworldcup.mytypeworldcup.global.util.CustomAuthorityUtils;
 import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,7 @@ import java.util.List;
 public class RefreshService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
 
     @Transactional
     public void saveRefreshToken(String email, String refreshToken, Date expiryDate) {
@@ -62,6 +65,33 @@ public class RefreshService {
         response.addHeader("Authorization", jwtTokenizer.delegateAccessToken(claimsToMember(claims)));
     }
 
+    public String refreshAccessToken(Cookie refreshTokenCookie, HttpServletResponse response) {
+        // 리프레쉬토큰을 찾는다
+        // 존재하지않으면 예외발생
+        RefreshToken refreshToken = findVerifiedRefreshToken(refreshTokenCookie.getValue());
+
+        // 유효기간확인
+        // 유효기간 만료됬을경우 디비에서 토큰삭제 및 브라우저 토큰 삭제
+        if (refreshToken.getExpiryDate().toInstant().isBefore(Instant.now())) { // 토큰의 만료일과 현재시간을 비교
+            refreshTokenRepository.delete(refreshToken);
+            refreshTokenCookie.setValue("");
+            refreshTokenCookie.setPath("/");
+            refreshTokenCookie.setMaxAge(0);
+            response.addCookie(refreshTokenCookie);
+            // 로그인 유도
+            throw new BusinessLogicException(AuthExceptionCode.REFRESH_TOKEN_EXPIRED);
+        }
+
+        // 새로운 액세스토큰생성 및 리턴
+        Member member = Member.builder()
+                .email(refreshToken.getEmail())
+                .roles(authorityUtils.createRoles(refreshToken.getEmail()))
+                .build();
+        String accessToken = jwtTokenizer.delegateAccessToken(member);
+
+        return accessToken;
+    }
+
     @Transactional
     public void logout(String email) {
         RefreshToken refreshToken = findVerifiedRefreshTokenByEmail(email);
@@ -71,6 +101,12 @@ public class RefreshService {
     @Transactional(readOnly = true)
     private RefreshToken findVerifiedRefreshTokenByEmail(String email) {
         return refreshTokenRepository.findById(email)
+                .orElseThrow(() -> new BusinessLogicException(AuthExceptionCode.REFRESH_TOKEN_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    private RefreshToken findVerifiedRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByToken(refreshToken)
                 .orElseThrow(() -> new BusinessLogicException(AuthExceptionCode.REFRESH_TOKEN_NOT_FOUND));
     }
 
