1d2c3a4559e4e2cbd9b3b6fead0bb41bb0c31d86
Ksiyeong <yuiop2767@naver.com>
2023-08-26 00:38:11 +0900

    [refactor] LikeService
    서비스 로직 인터페이스화
     - LikeServiceImpl 추가
     - LikeServiceImplTest 변경

---
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/like/service/LikeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.mytypeworldcup.mytypeworldcup.domain.like.service;
+
+import com.mytypeworldcup.mytypeworldcup.domain.comment.entity.Comment;
+import com.mytypeworldcup.mytypeworldcup.domain.like.entity.Like;
+import com.mytypeworldcup.mytypeworldcup.domain.like.exception.LikeExceptionCode;
+import com.mytypeworldcup.mytypeworldcup.domain.like.repository.LikeRepository;
+import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
+import com.mytypeworldcup.mytypeworldcup.global.error.BusinessLogicException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikeServiceImpl implements LikeService {
+    private final LikeRepository likeRepository;
+
+    @Override
+    public void createLike(Member member, Comment comment) {
+        verifyExistsLike(member, comment);
+        likeRepository.save(Like.builder().member(member).comment(comment).build());
+    }
+
+    @Override
+    public void deleteLike(Member member, Comment comment) {
+        Like like = findVerifiedLike(member, comment);
+        likeRepository.delete(like);
+    }
+
+    @Transactional(readOnly = true)
+    private void verifyExistsLike(Member member, Comment comment) {
+        Optional<Like> optionalLike = likeRepository.findByMemberAndComment(member, comment);
+        if (optionalLike.isPresent()) {
+            throw new BusinessLogicException(LikeExceptionCode.LIKE_EXISTS);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    private Like findVerifiedLike(Member member, Comment comment) {
+        return likeRepository.findByMemberAndComment(member, comment)
+                .orElseThrow(() -> new BusinessLogicException(LikeExceptionCode.LIKE_NOT_FOUND));
+    }
+
+}
