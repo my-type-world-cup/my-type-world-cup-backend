c2c6f0eed56111d7245bdf6e0b86cebc5d020a52
Ksiyeong <yuiop2767@naver.com>
2023-08-26 00:53:57 +0900

    [refactor] MemberService
    서비스 로직 인터페이스화
     - MemberServiceImpl 추가
     - MemberServiceImplTest 변경

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/member/service/MemberService.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/member/service/MemberService.java
@@ -1,95 +1,20 @@
 package com.mytypeworldcup.mytypeworldcup.domain.member.service;
 
-import com.mytypeworldcup.mytypeworldcup.domain.member.dto.MemberMapper;
 import com.mytypeworldcup.mytypeworldcup.domain.member.dto.MemberPatchDto;
 import com.mytypeworldcup.mytypeworldcup.domain.member.dto.MemberResponseDto;
 import com.mytypeworldcup.mytypeworldcup.domain.member.entity.Member;
-import com.mytypeworldcup.mytypeworldcup.domain.member.exception.MemberExceptionCode;
-import com.mytypeworldcup.mytypeworldcup.domain.member.repository.MemberRepository;
-import com.mytypeworldcup.mytypeworldcup.global.error.BusinessLogicException;
-import com.mytypeworldcup.mytypeworldcup.global.util.CustomAuthorityUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+public interface MemberService {
 
-@Transactional
-@Service
-@RequiredArgsConstructor
-public class MemberService {
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final CustomAuthorityUtils authorityUtils;
-    private final MemberMapper memberMapper;
+    Member createMember(Member member);
 
-    public Member createMember(Member member) {
-        verifyExistsEmail(member.getEmail());
+    //    Member findMember(String email, ProviderType providerType);
+    MemberResponseDto updateMember(String email, MemberPatchDto memberPatchDto);
 
-        // password 암호화
-        String encryptedPassword = passwordEncoder.encode(member.getPassword());
-        member.setPassword(encryptedPassword);
+    MemberResponseDto findMemberByEmail(String email);
 
-        // UserRole 세팅
-        List<String> roles = authorityUtils.createRoles(member.getEmail());
-        member.setRoles(roles);
+    Long findMemberIdByEmail(String email);
 
-        return memberRepository.save(member);
-    }
+    Member findVerifiedMemberByEmail(String email);
 
-    /*
-    public Member findMember(String email, ProviderType providerType) {
-        Optional<Member> optionalMember = memberRepository.findByEmail(email);
-        if (optionalMember.isPresent()) {
-            if (optionalMember.get().getProviderType() == providerType) {
-                return optionalMember.get();
-            } else {
-                throw new BusinessLogicException(MemberExceptionCode.경고합니다);
-            }
-        } else {
-            Member member = Member.builder().email(email)
-                    .nickname("잠시테스트")
-                    .password("oauth") //TODO 방법 생각해내라
-                    .providerType(providerType)
-                    .roles(List.of("ROLE_USER")) //TODO 관련 로직 만드세요!!
-                    .build();
-            return memberRepository.save(member);
-        }
-    }
-     */
-
-    public MemberResponseDto updateMember(String email, MemberPatchDto memberPatchDto) {
-        Member member = findVerifiedMemberByEmail(email);
-
-        Optional.ofNullable(memberPatchDto.getNickname()).ifPresent(data -> member.setNickname(data));
-
-        return memberMapper.memberToMemberResponseDto(member);
-    }
-
-    @Transactional
-    public MemberResponseDto findMemberByEmail(String email) {
-        Member member = findVerifiedMemberByEmail(email);
-        return memberMapper.memberToMemberResponseDto(member);
-    }
-
-    @Transactional(readOnly = true)
-    public Long findMemberIdByEmail(String email) {
-        return findVerifiedMemberByEmail(email).getId();
-    }
-
-    @Transactional(readOnly = true)
-    public Member findVerifiedMemberByEmail(String email) {
-        return memberRepository.findByEmail(email)
-                .orElseThrow(() -> new BusinessLogicException(MemberExceptionCode.MEMBER_NOT_FOUND));
-    }
-
-    @Transactional(readOnly = true)
-    private void verifyExistsEmail(String email) {
-        Optional<Member> optionalMember = memberRepository.findByEmail(email);
-        if (optionalMember.isPresent()) {
-            throw new BusinessLogicException(MemberExceptionCode.MEMBER_EXISTS);
-        }
-    }
 }
