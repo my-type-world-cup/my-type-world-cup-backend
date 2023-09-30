6836a777162a78dd8d7e41e00293069459c2aded
Ksiyeong <philip2767@gmail.com>
2023-09-30 21:15:52 +0900

    [refactor] MatchDto 제약조건 추가
    id : 1이상
    matchUpGameCount : 1이상
    winCount : 0이상

--- src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/dto/MatchDto.java
+++ src/main/java/com/mytypeworldcup/mytypeworldcup/domain/candidate/dto/MatchDto.java
@@ -1,12 +1,16 @@
 package com.mytypeworldcup.mytypeworldcup.domain.candidate.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class MatchDto {
+    @Min(1)
     private Long id;
+    @Min(1)
     private Integer matchUpGameCount;
+    @Min(0)
     private Integer winCount;
 
     @Builder
